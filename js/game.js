fde25ee0a9da4b0399ca01aae05de8531d7d4797
JoveX <xuyq1112@gmail.com>
2015-03-12 20:53:09 +0800

    translate some englist to chinese

--- js/game.js
+++ js/game.js
@@ -179,7 +179,7 @@ var Game = new (function() {
   function showSizes() {
     onHomeScreen = false;
     showGame();
-    $('#boardsize').html('<span>Select a size</span>');
+    $('#boardsize').html('<span>请选择模式</span>');
     $('#menugrid').removeClass('hidden');
     $('#board').addClass('hidden');
     $('#bar [data-action]').not('[data-action="back"]').hide();
